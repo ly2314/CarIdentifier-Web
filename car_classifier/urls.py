27437bb32a5ac50cdda51b120f2e8f9659b538b9
ly2314 <ly2314@gmail.com>
2017-11-26 14:51:31 -0800

    modified urls.py

--- car_classifier/urls.py
+++ car_classifier/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', include('classify.urls')),
     url(r'^image/$', include('image.urls')),
+    url(r'^feedback/$', include('feedback.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
